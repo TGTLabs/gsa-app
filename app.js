73bc8c16181d37c821f54dc2e85c4eca09ee2e52
Luke Gesior <luke.gesior@target.com>
2014-09-22 10:16:47 -0500

    change 404 page to survey list

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var morgan = require('morgan');
 var compress = require('compression');
 var path = require('path');
 var exphbs  = require('express-handlebars');
+var request = require('request');
 
 var app = express();
 app.set('title', thisPackage.description);
@@ -39,8 +40,18 @@ app.get('/polar/set/:id', function (req, res) {
 });
 
 app.use(function(req, res, next){
-  res.render('404', { status: 404, url: req.url });
+  // res.render('404', { status: 404, url: req.url });
+  request('https://polarb.com/api/v4/users/nicolenetland/polls_created?limit=50', function (error, response, body) {
+  var pollSummary = {};
+  if (!error && response.statusCode == 200) {
+  var bodyParsed = JSON.parse(body);
+  console.log(bodyParsed);
+  res.render('404', { res: bodyParsed});
+  }
+  });
 });
+
+
 
 var port = process.env.PORT || 5000 || 9000;
 app.listen(port, function () {
